cf9a4a43f321a0742fdabce7b4560297c882ff86
Helcon Mabesa <helcon.mabesa@foxnews.com>
2020-05-02 21:05:18 -0400

    update tests

--- test.js
+++ test.js
@@ -3,104 +3,107 @@ const iter = 100000;
 const log = console.log;
 
 (() => {
-    console.time('string concat');
+    console.time('test: string concat');
     let s = '';
     for (let i = 0; i < iter; i++) {
         s += i;
     }
-    console.timeEnd('string concat');
-    log('\n');
+    console.timeEnd('test: string concat');
 })();
 
 (() => {
-    console.time('string array');
+    console.time('test: string array');
     let s = [];
     for (let i = 0; i < iter; i++) {
         s.push(i);
     }
     s.join('');
-    console.timeEnd('string array');
-    log('\n');
+    console.timeEnd('test: string array');
 })();
 
 (() => {
-    console.time('randomizr.hash');
-    log('hash:', randomizr.hash());
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.hash();
-    }
-    console.timeEnd('randomizr.hash');
-    log('\n');
+    console.time('finished');
+    log('\nhash:', randomizr());
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.range1');
-    log('range[0-9]:', randomizr.range());
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.range();
-    }
-    console.timeEnd('randomizr.range1');
-    log('\n');
+    console.time('finished');
+    log('\nhash [ using generate() ]:', randomizr.generate());
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.range2');
-    log(`range[1-${iter}]:`, randomizr.range(0, iter));
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.range(0, iter);
-    }
-    console.timeEnd('randomizr.range2');
-    log('\n');
+    console.time('finished');
+    log('\nhash [ generate() + camelcase ]:', randomizr.generate(64, {
+        camelCase: true
+    }));
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.generate');
-    log('generate:', randomizr.generate());
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.generate();
-    }
-    console.timeEnd('randomizr.generate');
-    log('\n');
+    console.time('finished');
+    log('\nrange[ 0-9 ]:', randomizr.range());
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.generate.add');
-    log('generate add:', randomizr.generate(32, {
+    console.time('finished');
+    log(`\nrange[ 1-${iter} ]:`, randomizr.range(0, iter));
+    console.timeEnd('finished');
+})();
+
+(() => {
+    console.time('finished');
+    log('\ngenerate:', randomizr.generate());
+    console.timeEnd('finished');
+})();
+
+(() => {
+    console.time('finished');
+    log('\ngenerate [ add ]:', randomizr.generate(32, {
         add: '~`!@#$%^&*()_+?><,.;:\'"[{]}\\|"\'\`'
     }));
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.generate(32, {
-            add: '~`!@#$%^&*()_+?><,.;:\'"[{]}\\|"\'\`'
-        });
-    }
-    console.timeEnd('randomizr.generate.add');
-    log('\n');
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.generate.custom2');
-    log('generate custom [1 or 0]:', randomizr.generate(32, {
+    console.time('finished');
+    log('\ngenerate custom [ 1/0 ]:', randomizr.generate(32, {
         custom: '10'
     }));
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.generate(32, {
-            custom: '10'
-        });
-    }
-    console.timeEnd('randomizr.generate.custom2');
-    log('\n');
+    console.timeEnd('finished');
 })();
 
 (() => {
-    console.time('randomizr.generate.custom1');
-    log('generate custom:', randomizr.generate(32, {
+    console.time('finished');
+    log('\ngenerate [ custom chars(32) ]:', randomizr.generate(32, {
         custom: '~`!@#$%^&*()_+?><,.;:\'"[{]}\\|"\'\`'
     }));
-    for (let i = 0; i < iter; i++) {
-        let s = randomizr.generate(32, {
-            custom: '~`!@#$%^&*()_+?><,.;:\'"[{]}\\|"\'\`'
-        });
-    }
-    console.timeEnd('randomizr.generate.custom1');
-    log('\n');
+    console.timeEnd('finished');
+})();
+
+(() => {
+    console.time('finished');
+    log('\ngenerate [ alpha(32) ]):', randomizr.generate(32, {
+        type: 'alpha'
+    }));
+    console.timeEnd('finished');
+})();
+
+(() => {
+    console.time('finished');
+    log('\ngenerate custom [ alpha(32) + camelCase ]:', randomizr.generate(32, {
+        type: 'alpha',
+        camelCase: true
+    }));
+    console.timeEnd('finished');
+})();
+
+(() => {
+    console.time('finished');
+    log('\ngenerate custom [ numeric(32) ]:', randomizr.generate(32, {
+        type: 'numeric'
+    }));
+    console.timeEnd('finished');
 })();
